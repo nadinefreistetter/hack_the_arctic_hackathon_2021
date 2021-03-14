57eb41abd0cd0f9b1ef085315764fcb0240c0ba7
Jelmergraaff <80599687+Jelmergraaff@users.noreply.github.com>
2021-03-14 02:27:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,90 +5,107 @@ import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 df = pd.read_csv('hta.csv')
 
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#fig = px.area(df, x="Year", y="Snow_Percent")
+# fig = px.area(df, x="Year", y="Snow_Percent")
 
 fig = px.line(df, x="Year", y="Snow_Percent", title='Placeholder')
 
 fig['layout']['yaxis']['autorange'] = "reversed"
 
-app.title = 'ChronosZoi' 
+app.title = 'ChronosZoi'
 
 app.layout = html.Div([
 
+    html.Img(src=app.get_asset_url('SvalbardIMG.jpg'), style={'width': '100%'}),
 
-    
+    html.Br(),
+
     html.H1("ChronosZoi 2021", style={'text-align': 'center'}),
 
+    html.Br(),
 
+    html.P("Welcome to this Web Application. With this tool, you can see what trends are going on in the Svalbard area."
+           " Here, you can select any year between 1976 and 2100, to see how the observed and forecasted trends are for the temperature, snow depth and water quality.",
+           style={'text-align': 'center'}),
 
-    html.Div([
-    dcc.Input(
-        id='input-field',
-        type='text',
-        placeholder='Enter snow depth',
-    ),
-    ], style = {'width': '100%', 'align-items': 'center', 'justify-content': 'center'}),
-
-
-    html.H3(id='output-header', style={'text-align': 'center'}),
-
-
-    dcc.Graph(id='my_bee_map', figure=fig, style={'text-align': 'center'}),
-
-    html.Div([
-    daq.Thermometer(
-        label='Sea Surface Temperature',
-        labelPosition='top',
-        id='my-thermometer',
-        min=0,
-        max=12,
-        showCurrentValue=True,
-        color='red',
-        style={
-            'margin-bottom': '5%',
-            'backgroundColor': 'transparent'
-        }
-    ),
-    ], style = {'width': '100%', 'display': 'inline-block', 'align-items': 'left', 'justify-content': 'left'}),
-
-
-    html.Div([
-    daq.Gauge(
-    id='my-gauge',
-    color={'gradient':True, "ranges":{"green":[0.39, 1],"yellow":[0.24, 0.39],"red":[0, 0.24]}},
-    #color="#9B51E0",
-    label='Chlorophyll',
-    max=1,
-    min=0,
-    ),
-    ], style = {'width': '100%', 'display': 'inline-block', 'align-items': 'right', 'justify-content': 'right'}),
-
-  
-
-    html.Div(id='slider-output-container'),
+    html.Br(),
+    html.Br(),
 
     daq.Slider(
         id='thermometer-slider',
         min=1976,
         max=2100,
         value=1976,
-        handleLabel={"showCurrentValue": True,"label": "Year"},
+        handleLabel={"showCurrentValue": True, "label": "Year"},
         step=1,
-        size=1700,
-    )
+        size=1500,
+    ),
+
+    html.Br(),
+    html.Br(),
+
+    html.P("The average amount of snowfall in the Svalbard area is decreasing. "
+           "Here below, you can add any value, representing the snow depth in the old climate (around 1976)."
+           "The text below will show how that number has changed over the years.",
+           style={'text-align': 'center'}),
+
+    html.Div([
+        dcc.Input(
+            id='input-field',
+            type='text',
+            placeholder='Enter snow depth (cm)',
+            style={'width': '100%', 'align-items': 'center', 'justify-content': 'center', 'text-align': 'center'}
+        ),
+    ], style={'width': '100%', 'align-items': 'center', 'justify-content': 'center'}),
+
+    html.H3(id='output-header', style={'text-align': 'center'}),
+
+    dcc.Graph(id='my_bee_map', figure=fig, style={'text-align': 'center'}),
+
+    html.Img(src=app.get_asset_url('SvalbardIMG2.jpg'), style={'width': '100%'}),
+
+    html.Br(),
+    html.Br(),
+
+    html.Div([
+        daq.Thermometer(
+            label='Sea Surface Temperature',
+            labelPosition='top',
+            id='my-thermometer',
+            min=0,
+            max=12,
+            showCurrentValue=True,
+            color='red',
+            style={
+                'margin-bottom': '5%',
+                'backgroundColor': 'transparent'
+            }
+        ),
+    ], style={'width': '100%', 'display': 'inline-block', 'align-items': 'left', 'justify-content': 'left'}),
+
+    html.Img(src=app.get_asset_url('SvalbardIMG3.jpg'), style={'width': '100%'}),
+
+    html.Div([
+        daq.Gauge(
+            id='my-gauge',
+            color={'gradient': True, "ranges": {"green": [0.39, 1], "yellow": [0.24, 0.39], "red": [0, 0.24]}},
+            # color="#9B51E0",
+            label='Chlorophyll',
+            max=1,
+            min=0,
+        ),
+    ], style={'width': '100%', 'display': 'inline-block', 'align-items': 'right', 'justify-content': 'right'}),
+
+    html.Div(id='slider-output-container'),
+
 
 
 ])
-
-
 
 
 @app.callback(
@@ -105,18 +122,18 @@ def update_thermometer(value):
     d = ''
     return b, c, d
 
+
 @app.callback(
-    dash.dependencies.Output('output-header','children'),
-    [dash.dependencies.Input('input-field','value')]
+    dash.dependencies.Output('output-header', 'children'),
+    [dash.dependencies.Input('input-field', 'value')]
 )
 def update_header(prop):
     prop = float(prop)
     row = df[df['Year'] == yr]
     snow_percent = float(row['Snow_Decimals'].values)
-    res_str = str(round(prop*snow_percent, 2)) 
+    res_str = str(round(prop * snow_percent, 1))
     result = res_str + " cm in the year " + str(yr)
     return result
-
 
 
 if __name__ == '__main__':
